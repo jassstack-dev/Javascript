9c24a22a658f7fa656f1a8cd930cef83614f95f6
jass-vimal <vimaljass8@gmail.com>
2026-09-03 20:41:07 +0530

    dom start

---
+++ DOM/firstday.js
@@ -0,0 +1,21 @@
+// selection of element
+
+// --> querySelector -> only selector one element(pahala element) 
+//--> querySelectorDom  --> select multle element (select all element)
+
+const h1 = document.querySelector('h1')
+
+// when in the show you like html collection 
+// --> html collection -= live chalta haib 
+// nodelist - ye reload pr chalta hai 
+
+
+console.dir(h1)
+
+
+// changiing of html
+
+// ---> innerHTML, innerText
+
+//   --> innerHTML - whole tag ko chnager karta hai 
+// innertext => ye sirf text chnage karta hai 
