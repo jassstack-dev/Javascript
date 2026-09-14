a5c02495d22e668fe4eaa575366c5d17bfbdc573
jass-vimal <vimaljass8@gmail.com>
2026-09-14 23:06:41 +0530

    add a mini project that is Product Management Dashboard

---
+++ projectsz/Product Management Dashboard/app.js
@@ -0,0 +1,188 @@
+const  createBtn = document.querySelector('.createBtn')
+const  productBtn = document.querySelector('.seeProducts')
+const form = document.querySelector('.form')
+const products = document.querySelector('.products')
+const productForm = document.querySelector('form')
+const successPopup = document.querySelector('.prdCreated')
+
+
+
+
+
+createBtn.addEventListener('click', function(e){
+  
+    form.style.display = 'flex'
+     products.style.display = 'none'
+
+    
+     
+    
+})
+
+productBtn.addEventListener('click', function(){
+    products.style.display = 'flex'
+     form.style.display = 'none'
+})
+
+let productArr = JSON.parse(localStorage.getItem('products')) || []
+
+console.log(productArr)
+
+function ui(){
+    products.innerHTML = ""
+
+    productArr.forEach(function(e, index){
+        products.innerHTML += `<div class="product">
+            <div class="img">
+                <img src="${e.image}" alt="">
+            </div>
+            <div class="name">
+                <h2>${e.name}</h2>
+            </div>
+            <div class="description">
+                <p>${e.description}</p>
+            </div>
+            <div class="price-section">
+                <div class="price">
+                    $<span>${e.price}</span>
+                </div>
+                <div class="buyBtn" data-set = "${index}">
+                    Buy Now
+                </div>
+            </div>
+        </div>`
+    })
+
+
+       
+}
+
+ui()
+
+
+
+
+
+form.addEventListener('submit', function(e){
+e.preventDefault()
+
+let name = e.target[0].value
+let description = e.target[1].value
+let price = e.target[2].value
+let image = e.target[3].value
+
+let obj = {
+    name, description,price,image
+}
+
+productArr.push(obj)
+localStorage.setItem('products', JSON.stringify(productArr))
+
+// console.log(obj)
+
+
+
+        successPopup.style.display = 'flex'
+
+        setTimeout(function(){
+            successPopup.style.display = 'none'
+        },2000)
+
+ui()
+         
+})
+
+
+// see product in cart
+
+let cartProduct = []
+
+products.addEventListener('click', function(e){
+    if(e.target.classList.contains('buyBtn')){
+        let index = e.target.dataset.set
+
+        let selectProduct = productArr[index]
+
+        cartProduct.push(selectProduct)
+        console.log(cartProduct)
+    }
+})
+
+
+// add to cart
+
+const buyBtn = document.querySelectorAll('.buyBtn')
+const cartNumber = document.querySelector('.cart span')
+const cart = document.querySelector('.cart')
+
+
+let productInCart = 0
+
+buyBtn.forEach(function(btn){
+    
+btn.addEventListener('click', function(){
+
+
+productInCart += 1
+
+cartNumber.textContent = productInCart
+
+
+
+})
+
+})
+
+const cartProducts = document.querySelector('.cart-products')
+
+function cartUi(){
+    cartProducts.innerHTML = ""
+    cartProduct.forEach(function(e){
+ cartProducts.innerHTML += ` <div class="cart-product">
+
+                <div class="cart-img">
+                    <img src="${e.image}" alt="">
+                </div>
+
+                <div class="cart-info">
+                    <h2>${e.name}</h2>
+                    <p>${e.description}</p>
+
+                    <div class="cart-price">
+                        $${e.price}
+                    </div>
+
+                    <div class="quantity">
+                        <button>-</button>
+                        <span>1</span>
+                        <button>+</button>
+                    </div>
+                </div>
+
+                <button class="remove-cart">
+                    Remove
+                </button>
+
+            </div>`
+
+    })
+}
+
+cart.addEventListener('click', function(){
+        products.style.display = 'none'
+     form.style.display = 'none'
+    cartUi()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ projectsz/Product Management Dashboard/readme.md
@@ -0,0 +1,11 @@
+Product add — form se
+Product ko object mein store karna
+Saare products ko array mein rakhna
+map() se cards render
+Delete product → filter() / splice()
+Edit product → find()
+Search product
+Category filter
+Total products / total price calculate karna
+localStorage mein save karna
+Page reload hone par data wapas lana
